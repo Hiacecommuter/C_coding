75b96b469a3cd9c4a95c76c7546384f03b137ffe
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-03-31 09:38:19 +1100

    Create CpluplusSyntax.cpp

---
+++ CpluplusSyntax.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string> //cstring for c string functions
+using namespace std;
+
+int main(){
+
+// define a string
+  
+string Cpp_string1 = 'hello!'; // string class
+  
+
+  
+
+  
+}
