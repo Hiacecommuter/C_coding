80e8231e7b0af2bfe23d902178fce9d34ed219a3
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-03-31 09:35:51 +1100

    Create Csyntax.c

---
+++ Csyntax.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+// define a string
+char C_string1[] = 'hello!';
+char C_string2[7] = {'H', 'e', 'l', 'l', 'o', '!' ,'\0'}; // from stdio.h
+  
+// string functions from string.h, including copy, concatnate, search, address, compare
+  
+  
+  
+}
