96882ebfa6286be86761941ba9ba734efc7c2d79
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-03-31 12:23:47 +1100

    Update Csyntax.c

--- Csyntax.c
+++ Csyntax.c
@@ -8,6 +8,20 @@ char C_string1[] = 'hello!';
 char C_string2[7] = {'H', 'e', 'l', 'l', 'o', '!' ,'\0'}; // from stdio.h
   
 // string functions from string.h, including copy, concatnate, search, address, compare
+
+// input and output
+char a;
+char read_word[100];
+char read_line[100];
+
+scanf("%c", &a);
+scanf("%s", read_word);
+scanf("\n");
+scanf("%[^\n]%*c",read_line);
+
+printf("%c\n", a);
+printf("%s\n", read_word);
+printf("%s", read_line);
   
   
   
