44091db9080168b7d8e88b935d1ac056f6035565
Hiacecommuter <82562175+Hiacecommuter@users.noreply.github.com>
2023-03-31 22:08:11 +1100

    Update Csyntax.c

--- Csyntax.c
+++ Csyntax.c
@@ -7,6 +7,11 @@ int main(){
 char C_string1[] = 'hello!';
 char C_string2[7] = {'H', 'e', 'l', 'l', 'o', '!' ,'\0'}; // from stdio.h
   
+/*In C and C++ the single quote is used to identify the single character, 
+and double quotes are used for string literals. 
+A string literal “x” is a string, it is containing character ‘x’ and a null terminator ‘\0’. 
+So “x” is two-character array in this case.*/
+  
 // string functions from string.h, including copy, concatnate, search, address, compare
 
 // input and output
